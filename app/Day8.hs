a5561d37e2487859659fc4d5884dcee13b34a73c
Arti Schmidt <artischmidt3@gmail.com>
2023-07-17 00:38:07 -0700

    Complete day 8

--- app/Day8.hs
+++ app/Day8.hs
@@ -1,8 +1,9 @@
-module Day8 where
+module Day8 () where
 
 import Data.List
 import Data.Char (digitToInt)
 
+-- visibility facing to the right
 visibleRow :: [Int] -> [Bool]
 visibleRow heightmap =
   zipWith (>) heightmap ((-1) : runningMax)
@@ -19,7 +20,28 @@ visible heightmap =
     visibleTop = transpose . map visibleRow . transpose $ heightmap
     visibleBottom = transpose . reverse' . map visibleRow . reverse' . transpose $ heightmap
 
+-- viewing distance facing to the right
+distRow :: [Int] -> [Int]
+distRow hs =
+  map (\xs -> dist (head xs) (tail xs)) (init $ tails hs)
+  where
+    dist h hs =
+      let (visible, invisible) = span (<h) hs
+      in length visible + (if null invisible then 0 else 1)
+
+dist :: [[Int]] -> [[Int]]
+dist heightmap =
+  distLeft ||* distRight ||* distTop ||* distBottom
+  where
+    (||*) = zipWith (zipWith (*))
+    reverse' = map reverse
+    distLeft = map distRow heightmap
+    distRight = reverse' . map distRow . reverse' $ heightmap
+    distTop = transpose . map distRow . transpose $ heightmap
+    distBottom = transpose . reverse' . map distRow . reverse' . transpose $ heightmap
+
 main :: IO ()
 main = do
   heightmap <- map (map digitToInt) . lines <$> getContents
   print $ sum . map (length . filter id) . visible $ heightmap
+  print $ maximum . map maximum . dist $ heightmap
