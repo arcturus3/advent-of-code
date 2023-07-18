fbe18bd607caae73f1d89b796b28328cac6af586
Arti Schmidt <artischmidt3@gmail.com>
2023-07-18 00:11:59 -0700

    Complete day 9 part 2

--- app/Day9.hs
+++ app/Day9.hs
@@ -10,25 +10,34 @@ delta 'D' = (0, -1)
 delta 'L' = (-1, 0)
 delta 'R' = (1, 0)
 
--- old head offset from tail -> head movement -> new head offset from tail
-transition :: (Int, Int) -> (Int, Int) -> (Int, Int)
-transition (x, y) (dx, dy)
-  | x' < -1 = (-1, 0)
-  | x' > 1 = (1, 0)
-  | y' < -1 = (0, -1)
-  | y' > 1 = (0, 1)
-  | otherwise = (x', y')
-  where (x', y') = (x + dx, y + dy)
+moveTail :: (Int, Int) -> (Int, Int) -> (Int, Int)
+moveTail (hx, hy) (tx, ty) =
+  if (dx, dy) == (dx', dy')
+    then (tx, ty)
+    else (tx + dx', ty + dy')
+  where
+    (dx, dy) = (hx - tx, hy - ty)
+    (dx', dy') = (clamp (-1, 1) dx, clamp (-1, 1) dy)
+
+moveRope :: [(Int, Int)] -> (Int, Int) -> [(Int, Int)]
+moveRope rope (dx, dy) =
+  scanl moveTail (hx', hy') (tail rope)
+  where
+    (hx, hy) = head rope
+    (hx', hy') = (hx + dx, hy + dy)
+
+solve :: [(Char, Int)] -> Int -> Int
+solve moves length =
+  size . fromList $ positions
+  where
+    deltas = map delta . concatMap (uncurry $ flip replicate) $ moves
+    rope = replicate length (0, 0)
+    ropes = scanl moveRope rope deltas
+    positions = map last ropes
 
 main :: IO ()
 main = do
-  let parseLine line = let items = words line in (items !! 0 !! 0, read (items !! 1) :: Int)
+  let parseLine line = let [move, times] = words line in (head move, read times :: Int)
   input <- map parseLine . lines <$> getContents
-  let deltas = map delta . concatMap (uncurry $ flip replicate) $ input
-  let headPositions = scanl add (0, 0) deltas
-  let offsets = scanl transition (0, 0) deltas
-  let tailPositions = zipWith sub headPositions offsets
-  print . size . fromList $ tailPositions
-  where
-    add (a, b) (c, d) = (a + c, b + d)
-    sub (a, b) (c, d) = (a - c, b - d)
+  print $ solve input 2
+  print $ solve input 10
